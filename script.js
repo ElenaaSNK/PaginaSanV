7b6ab703b753f5a3cbbaf76326ea3edb9d679dfa
ElenaaSNK <elenaelillo@gmail.com>
2026-02-06 13:47:17 +0100

    Base de la pagina

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const btnSi = document.getElementById("btnSi");
+const btnNo = document.getElementById("btnNo");
+const mensaje = document.getElementById("mensaje");
+
+btnNo.addEventListener("mouseenter",(e)=>{
+    mensaje.textContent = "EEEEH QUE TE CREES QUE HACES?"
+})
